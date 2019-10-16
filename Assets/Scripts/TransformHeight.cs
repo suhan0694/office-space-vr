791d9f34d61842858b532cd436c2537739d2aef3
suhan0694 <snath6@uic.edu>
2019-10-16 11:16:55 -0500

    Height Change

---
+++ Assets/Scripts/TransformHeight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformHeight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(0,2f,0);
+    }
+
+    public void updateShortHeight() {
+        transform.position = new Vector3(transform.position.x, 0.5f, transform.position.y);
+    }
+
+    public void updateNormalHeight()
+    {
+        transform.position = new Vector3(transform.position.x, 1.5f, transform.position.y);
+    }
+
+    public void updatetallHeight()
+    {
+        transform.position = new Vector3(transform.position.x, 2.5f, transform.position.y);
+    }
+}
